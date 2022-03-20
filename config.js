a31abe281d9b6e207ca6b963399390696ceaa384
Madhavi R <madhavi.divakar@gmail.com>
2022-03-20 07:52:46 -0400

    facebook oath

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
     'facebook': {
-        clientId: '672415300464467',
-        clientSecret: 'c84905620cd33bdac7011e4e5789b6ef'
+        clientId: 'YOUR FACEBOOK APP ID HERE',
+        clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
     }
 }
